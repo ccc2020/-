536db8d7399f768865706a201dcd9735c4b4d8c1
ccc2020 <60969679+ccc2020@users.noreply.github.com>
2020-02-27 11:42:20 +0800

    Add files via upload

---
+++ isprime.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int isPrime(unsigned  short n)
+{     int i,k=1;
+    for( i=2;i<n/2;i++)
+      { if(n%i==0) k=0 ;}
+        return k;
+ }    
+
+int main()
+{
+    unsigned short a;
+    cin>>a;
+    if (isPrime(a))
+        cout<<"Y"<<endl;
+    else
+        cout<<"N"<<endl;
+}
+
